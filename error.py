90834cd1f05aadabf057ac55efbd7663875d5e77
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-06 07:24:03 +0200

    feat:add solution except error read file add to list

--- error.py
+++ error.py
@@ -2,3 +2,16 @@ import sys
 url=['text.txt','text2.txt','text3.txt','text4.txt']
 list_defect =[]
 list_info =[]
+for path in url:
+    try:
+        r=open(path)
+        list_info.append(r.read())
+        print('try',list_info)
+    except Exception:
+        list_defect.append(path)
+        print('error',list_defect)
+        continue
+    
+
+
+
