41c627448b51085784f89ecb6e4f5b84f9cfe13d
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-07 08:33:43 +0200

    feat:add yield fix

--- function_generator.py
+++ function_generator.py
@@ -1,9 +1,16 @@
+# def some():
+#     list_text=[]
+#     with open('text.txt', encoding = 'utf-8' ) as r:
+#        for i in r:
+#             list_text.append(i)
+#     return list_text        
+
+
+
 def some():
-    list_text=[]
     with open('text.txt', encoding = 'utf-8' ) as r:
-       for i in r:
-            list_text.append(i)
-    return list_text        
+        for x in r:
+            yield x
 
 for i in some():
     print(i.split())
