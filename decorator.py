cf874f537f01e8387202ec769e2423216ff9e84b
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-07 07:24:02 +0200

    feat: example decor except error for many def

--- decorator.py
+++ decorator.py
@@ -1,17 +1,42 @@
+# def decor(f):
+#     def wrapper():
+
+#         print('Code decor')
+#         f()         # The body function(def make) is started here. 
+#         print('Code 2 decor')
+#     return wrapper
+
+
+# @decor #make=decor(make)
+# def make():
+#     enter=input('Enter somesing...')
+#     print(enter)
+
+# print('start')
+# make()
+# decor This poundation is wrapping the necessary function. To simplify the code of each function.
+
+
+
 def decor(f):
     def wrapper():
-
-        print('Code decor')
-        f()         # The body function(def make) is started here. 
-        print('Code 2 decor')
+        try:
+            h=f() 
+        except Exception: 
+            print('Repeat input...')
+            h=f()         
+        return h
     return wrapper
 
 
 @decor #make=decor(make)
 def make():
-    enter=input('Enter somesing...')
-    print(enter)
+    enter=float(input('Enter number: '))
+    return enter
+@decor #make2=decor(make2)
+def make2():
+    enter=float(input('Enter number next: '))
+    return enter
 
-print('start')
+make2()
 make()
-# decor This poundation is wrapping the necessary function. To simplify the code of each function.
