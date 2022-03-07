74a43e01479a40dc8d93742e9297aab9e23bdf11
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-07 07:58:04 +0200

    feat:add next

--- generator.py
+++ generator.py
@@ -1,15 +1,6 @@
-h=[5,5,6,7,4,2,3,5,66,66]
-new_h=[]
-for i in h:
-    new_h.append(i*3)
-print(new_h) 
+h=[5,5,6,7,4,2,3,5,66,66,-4,-46,-1]
 
-new_h2=[x*3 for x in h]
-print(new_h2) #We generate a list
-
-new_h3={x*3 for x in h}
-print(new_h3) #generable multitude
-
-new_h4={x:x*3 for x in h}
-print(new_h4) #generable dictionary
-
+z=(x*3 for x in  h)         #Expression generator
+# for i in z:
+#     print(i)
+print(next(z)) # To optimize the use of resources
