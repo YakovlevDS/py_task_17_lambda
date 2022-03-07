5b5fca73093c90595b1ed7e9ba5a4598a11975e9
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-07 09:05:08 +0200

    feat:add filter

--- lambda.py
+++ lambda.py
@@ -1,17 +1,18 @@
-import re
+# from dis import dis
+# def some(x):
+#     return x/5
 
+# var = lambda z: z/5 
 
-from dis import dis
-def some(x):
-    return x/5
-
-var = lambda z: z/5 
-
-print (some(4))
-print (dis(some))
-print (dis(var))
-print (var(4))
+# print (some(4))
+# print (dis(some))
+# print (dis(var))
+# print (var(4))
 
 list_of=[["Jon",23],["acj",20],["Tom",2]]
-r= sorted(list_of,key=lambda x:x[1])
-print (r)
+# r= sorted(list_of,key=lambda x:x[1])
+# print (r)
+
+
+x = list(filter(lambda x:x[1]>19, list_of))
+print (x)
