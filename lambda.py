600df07515b61a69435db11bbc85d4a27dee3c47
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-07 08:57:24 +0200

    feat:add lambda

---
+++ lambda.py
@@ -0,0 +1,17 @@
+import re
+
+
+from dis import dis
+def some(x):
+    return x/5
+
+var = lambda z: z/5 
+
+print (some(4))
+print (dis(some))
+print (dis(var))
+print (var(4))
+
+list_of=[["Jon",23],["acj",20],["Tom",2]]
+r= sorted(list_of,key=lambda x:x[1])
+print (r)
